1eb8113ce1b8093a4309193453ec58b30165b4dc
Arick Smith <kidwriter2020@gmail.com>
2016-09-21 12:33:12 -0600

    Made my UI app.

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var makeBlock = UIView(frame: CGRectMake(2, 34, 78,78))
+makeBlock.backgroundColor = UIColor.redColor()
+
+
+makeBlock.layer.cornerRadius = 20
+makeBlock.layer.borderWidth = 12
+makeBlock.layer.borderColor
+makeBlock
+var button = UIButton(frame: CGRectMake(34, 345, 56, 56))
+
+var Label
